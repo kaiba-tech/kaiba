7848c58b86774a606151c592a01be553801fbc89
Borgen <thomas@borgenit.no>
2020-08-20 21:31:49 +0200

    adds test that our schema.json is a valid draft7 schema (#44)

---
+++ tests/schema/test_validate_schema.py
@@ -0,0 +1,12 @@
+import json
+
+from jsonschema import Draft7Validator
+
+
+def test_validate_schema():
+    """Test that our schema.json is a valid draft7 schema."""
+    schema_data = {}
+    with open('mapmallow/schema.json', 'r') as schema_file:
+        schema_data = json.load(schema_file)
+
+    assert Draft7Validator.check_schema(schema_data) is None
