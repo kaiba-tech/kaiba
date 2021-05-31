74da2f08377c51897687172499832942eeb7910e
thomasborgen <thomasborgen91@gmail.com>
2021-05-31 21:55:48 +0200

    Migrates collection handlers to pydantic

--- kaiba/collection_handlers.py
+++ kaiba/collection_handlers.py
@@ -4,6 +4,7 @@ from returns.result import Failure, ResultE, Success, safe
 
 from kaiba.constants import ALIAS, PATH
 from kaiba.valuetypes import MapValue
+from kaiba.pydantic_schema import Iterable
 
 
 @safe
@@ -65,19 +66,22 @@ def fetch_list_by_keys(
     raise ValueError('Non list data found: ', str(collection))
 
 
-def iterable_data_handler(raw_data, paths) -> ResultE[list]:
-    """Iterate and create all combinations from list of paths."""
-    if not paths:
-        return Failure(ValueError('No paths'))
+def iterable_data_handler(
+    raw_data,
+    iterables: List[Iterable],
+) -> ResultE[list]:
+    """Iterate and create all combinations from list of iterables."""
+    if not iterables:
+        return Failure(ValueError('No iterables'))
 
-    path, rest = paths[0], paths[1:]
+    iterable, rest = iterables[0], iterables[1:]
 
     if not rest:
-        return create_iterable(raw_data, path)
+        return create_iterable(raw_data, iterable)
 
     my_list: list = []
 
-    for iterable in create_iterable(raw_data, path).unwrap():
+    for iterable in create_iterable(raw_data, iterable).unwrap():
 
         iterable_data_handler(iterable, rest).map(
             my_list.extend,
@@ -86,16 +90,19 @@ def iterable_data_handler(raw_data, paths) -> ResultE[list]:
     return Success(my_list)
 
 
-def create_iterable(input_data, iterable) -> ResultE[list]:
+def create_iterable(
+    input_data,
+    iterable: Iterable,
+) -> ResultE[list]:
     """Return set of set of data per entry in list at iterable[path]."""
     return fetch_list_by_keys(
         input_data,
-        iterable[PATH],
+        iterable.path,
     ).map(
         lambda collections: [
             {
                 **input_data,
-                **{iterable[ALIAS]: collection},
+                **{iterable.alias: collection},
             }
             for collection in collections
         ],
