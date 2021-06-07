c9ec9223ba710d7df02415e44d55aadffc6fbed8
thomasborgen <thomasborgen91@gmail.com>
2021-06-07 20:17:56 +0200

    Adds docstring.

--- tests/test_create_json_schema.py
+++ tests/test_create_json_schema.py
@@ -2,4 +2,5 @@ from kaiba.pydantic_schema import KaibaObject
 
 
 def test_create_jsonschema_from_model():
+    """Test that we can create jsonschema."""
     assert KaibaObject.schema_json(indent=2)
